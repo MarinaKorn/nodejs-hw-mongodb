051b2dc1d4e392f53f9b770866f289ddbb250a48
MarinaKorn <gecagem840@gmail.com>
2025-09-02 21:39:42 +0200

    3

--- src/utils/getEnv.js
+++ src/utils/getEnv.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export function getEnvVar(name, defaultValue) {
-  const value = process.env[name];
+  const value = proccess.env[name];
 
   if (value) return value;
   if (defaultValue) return defaultValue;
