be6f09949dcfa40ee219e58ebe4f48888efc2105
MarinaKorn <gecagem840@gmail.com>
2025-09-12 17:31:40 +0200

    2

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import cookieParser from 'cookie-parser';
 import { getEnvVar } from './utils/getEnv.js';
 import router from './routers/contacts.js';
-import { errorHandler } from './middlewares/errorHandler.js';
-import { notFoundErr } from './middlewares/notFoundError.js';
+import { errorHandler } from './middlewars/errorHandler.js';
+import { notFoundErr } from './middlewars/notFoundError.js';
+import { authRouter } from './routers/auth.js';
+import { UPLOAD_DIR } from './constants/index.js';
 
 const PORT = Number(getEnvVar('PORT'));
 
@@ -21,6 +24,13 @@ export async function setupServer() {
       },
     }),
   );
+
+  app.use(cookieParser());
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use(authRouter);
+
   app.use(router);
 
   app.use(notFoundErr);
